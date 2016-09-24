0d7f6667d65fef3ec209e35b24830f63a4c87920
Rodrigo Sieben Bordignon <rodrigo.sieben@pucpr.edu.br>
2016-09-23 22:46:12 -0300

    atualizacao servidor

--- SFML/servidor/exemplo/Jogo.h
+++ SFML/servidor/exemplo/Jogo.h
@@ -2,9 +2,13 @@
 #include <SFML\Graphics.hpp>
 #include "Megaman.h"
 #include "Mouse.h"
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
 class Jogo
 {
+
 private:
 	sf::RenderWindow m_janela;
 	sf::Time m_tempoPorFrame;
@@ -12,6 +16,9 @@ private:
 	Mouse m_mouse;
 
 public :
+	WSAData wsaData;
+	SOCKET servidorSocket;
+	sockaddr_in servidorEndereco;
 	Jogo(int largura,
 		 int altura,
 		 const char* titulo,
@@ -43,6 +50,16 @@ public :
 		sf::Clock relogio;
 		sf::Time tempoDesdeUltimaAtualizacao = sf::Time::Zero;
 		m_janela.setMouseCursorVisible(false);
+			
+		WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+		servidorSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+		servidorEndereco.sin_family = AF_INET;
+		servidorEndereco.sin_port = htons(9999);
+		InetPton(AF_INET, "0.0.0.0", &servidorEndereco.sin_addr.s_addr);
+
+		bind(servidorSocket, (SOCKADDR*)&servidorEndereco, sizeof(sockaddr_in));
 
 		while (m_janela.isOpen())
 		{
